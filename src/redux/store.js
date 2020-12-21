1e79f84be4117f246b74092fc30c55fc6537cd79
Martin Scebba <mascebba@gmail.com>
2020-12-21 05:43:36 -0300

    redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,16 +2,16 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { throttle } from 'lodash';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import rootReducer from './rootReducer';
 import { loadFromLocalStorage, saveToLocalStorage } from './localStorage';
 
 const middlewares = [thunk];
 
-if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger);
-}
+// if (process.env.NODE_ENV === 'development') {
+//   middlewares.push(logger);
+// }
 
 const store = createStore(
   rootReducer,
