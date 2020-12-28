771fd67836354a91ddac433a6f7c0974d0587b87
Martin Scebba <mascebba@gmail.com>
2020-12-28 03:47:12 -0300

    removed redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,15 +2,15 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import persistStore from 'redux-persist/es/persistStore';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import rootReducer from './rootReducer';
 
 const middlewares = [thunk];
 
-if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger);
-}
+// if (process.env.NODE_ENV === 'development') {
+//   middlewares.push(logger);
+// }
 
 export const store = createStore(
   rootReducer,
